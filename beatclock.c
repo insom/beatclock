bb43d89c1eca880adae88855d5768dc58a4d4e8f
Aaron Brady <aaron@insom.me.uk>
2022-11-23 21:59:48 -0500

    Write 0100 to LED backpack

---
+++ beatclock.c
@@ -0,0 +1,80 @@
+#include <error.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+
+const char numbers[][2] = {
+    {0x3f, 0x00}, // 0
+    {0x06, 0x00}, // 1
+    {0xdb, 0x00}, // 2
+    {0xcf, 0x00}, // 3
+    {0xe6, 0x00}, // 4
+    {0xed, 0x00}, // 5
+    {0xfd, 0x00}, // 6
+    {0x07, 0x00}, // 7
+    {0xff, 0x00}, // 8
+    {0xef, 0x00}, // 9
+};
+
+char buf[17] = {0};
+
+int configure_display(char *device) {
+  int file;
+
+  if ((file = open("/dev/i2c-1", O_RDWR)) < 0) {
+    perror("open");
+    exit(1);
+  }
+
+  int addr = 0x70;
+  if (ioctl(file, I2C_SLAVE, addr) < 0) {
+    perror("ioctl");
+    exit(1);
+  }
+
+  /* Get oscillated. */
+  buf[0] = 0x21;
+  if (write(file, buf, 1) != 1) {
+    perror("write");
+    exit(1);
+  }
+
+  /* Let there be light. */
+  buf[0] = 0x81;
+  if (write(file, buf, 1) != 1) {
+    perror("write");
+    exit(1);
+  }
+
+  return file;
+}
+
+void display(int file, int val, char dot) {
+  char strbuf[5];
+
+  sprintf(strbuf, "%04d", val);
+  for(char i = 0; i < 4; i++) {
+    int num = strbuf[i] - '0';
+    memcpy(buf + 1 + (i * 2), numbers[num], 2);
+  }
+
+  if(dot) {
+buf[8] |= 0x40;
+}
+
+  /* Load the data. */
+  buf[0] = 0x0;
+  if (write(file, buf, 17) != 17) {
+    perror("write");
+    exit(1);
+  }
+}
+
+void main(void) {
+  int file = configure_display("/dev/i2c-1");
+  display(file, 100, 1);
+}
